0bc962370b7f2b22cdec05e6b0a09306ad80985d
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-03 22:46:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,8 +37,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times = 1..7
-  for number in number_of_times
+  for number in 1..number_of_times
   puts phrase
 end
 end
